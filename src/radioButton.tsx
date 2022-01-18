e87b60d7d2f3cee806c736a0d8e7b36e559fee01
Mayank Budhiraja <mayankbudhiraja3@gmail.com>
2022-01-17 21:07:10 -0800

    init commit

---
+++ src/radioButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {RadioButtonProps} from './types';
+
+export default function RadiotButton({}: RadioButtonProps) {
+  return <View></View>;
+}
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type RadioButtonProps ={
+    color? : string;
+    disabled? : boolean;
+}
